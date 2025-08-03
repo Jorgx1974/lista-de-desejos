9097b93a83f900fde20f8bc038e232b0776eb063
Jorge Miguel Mesquita Flor <jorge12flor@gmail.com>
2025-08-02 21:56:36 -0300

    lista de desejos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (localStorage.getItem("token") == null) {
 
     alert("você precisa estar logado para acessar essa pagína");
-    window.location.href = "registro.html";
+    window.location.href = "login.html";
 }
 let userlogado = JSON.parse(localStorage.getItem("userlogado"));
 
@@ -11,6 +11,6 @@ logado.innerHTML = 'Olá $userLogado.nome)'
 function sair(){
 localStorage.removeItem("token");
 localStorage.removeItem("userLogado");
-window.location.href = "registro.html";
+window.location.href = "login.html";
 
 }
